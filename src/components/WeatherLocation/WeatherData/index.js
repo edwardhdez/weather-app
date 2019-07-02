86a520c0e001ce889b360480593b227e3cbda176
Desarrolladores <edwardhdez21@gmail.com>
2019-07-02 17:27:36 -0400

    Use state in WeatherLocation Component

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
-import {
-    SUN,
-} from './../../../constants/weathers';
+import PropTypes from 'prop-types';
 import './styles.css';
 
-const WeatherData = () => (
-    <div className="weatherDataCont">
-        <WeatherTemperature
-            temperature={20}
-            weatherState={SUN}
-        />
+const WeatherData = ({ data: { temperature, weatherState, humidity, wind } }) => {
 
-        <WeatherExtraInfo humidity={80} wind={'10 m/s'} />
-    </div>
-);
+    return (
+        <div className="weatherDataCont">
+            <WeatherTemperature
+                temperature={temperature}
+                weatherState={weatherState}
+            />
+            <WeatherExtraInfo humidity={humidity} wind={wind} />
+        </div>
+    );
+};
+
+WeatherData.propTypes = {
+    data: PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.string.isRequired,
+    }),
+};
 
 export default WeatherData;
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import {
+    SUN,
+    RAIN,
+} from './../../constants/weathers';
 import './styles.css';
 
-const WeatherLocation = () => (
-    <div className="weatherLocationCont">
-        <Location city={'Santiago'}></Location>
-        <WeatherData></WeatherData>
-    </div>
-);
+const location = 'Buenos Aires,ar';
+const api_key = '63d144ac1b9036b1d84edde2f153e86f';
+const api_base_url = 'https://samples.openweathermap.org/data/2.5/weather';
+
+const api_weather = `${api_base_url}?q=${location}&appid=${api_key}`;
+
+
+
+const data = {
+    temperature: 5,
+    weatherState: SUN,
+    humidity: 80,
+    wind: '10 m/s',
+}
+
+const data2 = {
+    temperature: 8,
+    weatherState: RAIN,
+    humidity: 70,
+    wind: '15 m/s',
+}
+
+class WeatherLocation extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            city: 'Santiago',
+            data: data,
+        };
+    }
+
+
+    handleUpdateClick = () => {
+        console.log("Actualizando");
+        this.setState({
+            city: 'Santiago!',
+            data: data2
+        });
+    }
+
+    render() {
+        const { city, data } = this.state;
+        return (
+            <div className="weatherLocationCont">
+                <Location city={city}></Location>
+                <WeatherData data={data}></WeatherData>
+                <button onClick={this.handleUpdateClick}>Update</button>
+            </div>
+        );
+    }
+
+}
+
 
 export default WeatherLocation;
